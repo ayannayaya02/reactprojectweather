dcc0407242f3da161122c16a16a0d3eee1ae7792
ayanna <ayannafofana.aaf@gmail.com>
2023-07-08 23:04:06 -0700

    basic css

--- src/weather.js
+++ src/weather.js
@@ -1,42 +1,55 @@
 import React from "react";
 import "./weather.css";
 
-export default function Weather(){
-return (
-  <div className="weather">
-    <form>
+export default function Weather() {
+  return (
+    <div className="weather">
+      <form>
         <div className="row">
-            <div className="col-9">
-  <input type="Search" placeholder="Search City..." className="form" />
-  </div>
-              <div className="col-3">
+          <div className="col-9">
+            <input
+              type="Search"
+              autoFocus="on"
+              placeholder="Search City..."
+              className="form"
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
+          </div>
+        </div>
+      </form>
 
-          <input type="submit" value="Search" className="btn btn-primary" />
-   </div>
-   </div>
-    </form>
-    <h1>Paris</h1>
+      <ul className="list">
+        <h1>Paris</h1>
 
-    <ul>
-      <li>Wednesday 07:00</li>
-      <li>Partly Cloudy</li>
-    </ul>
-    <div className="row">
-      <div className="col-6">
-        <img
-          src="https://openweathermap.org/img/wn/02d@2x.png"
-          alt="Partly Cloudy"
-        />
-        6 °C
+        <ul>Wednesday 07:00</ul>
+        <ul>Partly Cloudy</ul>
+      </ul>
+
+      <div className="row">
+        <div className="col-6">
+          <div className="clearfix">
+            <img
+              src="https://openweathermap.org/img/wn/02d@2x.png"
+              alt="Partly Cloudy"
+            />
+            <div className="float-left" />
+            <span className="temp">6</span>
+            <span className="unit">°C {""}</span>
+            <span className="fahrenheit">| ℉</span>
+          </div>
         </div>
-      <div className="col-6">
-          <ul>
-            <li>Percipitation: 15%</li>
-            <li>Humidity: 72%</li>
-            <li>Wind: 13km/h</li>
-          </ul>
-        </div>
-        </div>
-        </div>
-);
+        <ul>
+          <ul>Percipitation: 15%</ul>
+          <ul>Humidity: 72%</ul>
+          <ul>Wind: 13km/h</ul>
+        </ul>
+      </div>
+    </div>
+  );
 }
